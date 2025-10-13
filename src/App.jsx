60f43ab61b93739b6cba598ccaf8628fbf054acf
Dulanka Nimsara <dulankanimsaradn@gmail.com>
2025-10-13 19:12:03 +0530

    Admin Part

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Navbar from './components/Navbar';
-import AboutUsPage from './pages/Home/AboutUsPage';
-import ServicePage from './pages/Home/ServicePage';
 
+// Frontend
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home";
+import AboutUsPage from "./pages/Home/AboutUsPage";
+import ServicePage from "./pages/Home/ServicePage";
+
+// Admin
+import AdminLayout from "./admin/AdminLayout";
 
 const App = () => {
+  const isAdmin = window.location.pathname.startsWith("/admin");
+
   return (
     <Router>
-      <Navbar />
+      {!isAdmin && <Navbar />}
+
       <Routes>
+        {/* Frontend */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/services" element={<ServicePage />} />
+
+        {/* Admin */}
+        <Route path="/admin/*" element={<AdminLayout />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
