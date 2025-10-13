2ab92da1e5be95820d49d6f0b2cfecc3f71d579b
SandaliKodithuwakku <sandali768@gmail.com>
2025-10-13 11:00:01 +0530

    Add ContactHeroSection

---
+++ src/pages/Home/ContactUsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactHeroSection from './ContactHeroSection';
+
+const ContactUsPage = () => {
+  return (
+    <div>
+      <section id="contact" className="pt-[115px]">
+        <ContactHeroSection/>
+      </section>
+    </div>
+  );
+};
+
+export default ContactUsPage;
