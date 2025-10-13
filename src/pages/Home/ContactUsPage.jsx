26351e785303ae77368c86ad2e0eab9485e3357d
SandaliKodithuwakku <sandali768@gmail.com>
2025-10-13 11:10:42 +0530

    Add ContactInfoCards

--- src/pages/Home/ContactUsPage.jsx
+++ src/pages/Home/ContactUsPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactHeroSection from './ContactHeroSection';
+import ContactInfoCards from '../../components/ContactInfoCards';
 
 const ContactUsPage = () => {
   return (
     <div>
       <section id="contact" className="pt-[115px]">
         <ContactHeroSection/>
+        <ContactInfoCards/>
       </section>
     </div>
   );
